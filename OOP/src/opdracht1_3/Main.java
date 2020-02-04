bc7f4e65da741d6526afb856e6000b55145a1f5b
Ishara <ishara.macdonald@student.hu.nl>
2020-02-04 14:12:16 +0100

    Opdrachten gemaakt

---
+++ OOP/src/opdracht1_3/Main.java
@@ -0,0 +1,9 @@
+package opdracht1_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Rick", 3249890);
+
+        System.out.println(s1.toString());
+    }
+}
