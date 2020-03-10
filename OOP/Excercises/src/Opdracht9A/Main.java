a2c64b697c0f6fa79ec9d6ee8ca695e24dc50039
Ishara <ishara.macdonald@student.hu.nl>
2020-03-10 13:55:19 +0100

    Added Stuff

---
+++ OOP/Excercises/src/Opdracht9A/Main.java
@@ -0,0 +1,14 @@
+package Opdracht9A;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utils.euroBedrag(3.11314));
+        System.out.println(Utils.euroBedrag(3.11314, 1));
+        System.out.println(Utils.euroBedrag(3.11314, 2));
+        System.out.println(Utils.euroBedrag(3.11314, 3));
+        System.out.println(Utils.euroBedrag(3.11314, 4));
+        System.out.println(Utils.euroBedrag(3.11314, 5));
+
+        System.out.println(Utils.getCount());
+    }
+}
